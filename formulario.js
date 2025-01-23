724e87c8147e174351f2bdb678118e7982ce1198
sggt <sofiaggt2305@gmail.com>
2025-01-23 12:31:34 -0600

    revision de codigo 2

--- formulario.js
+++ formulario.js
@@ -1,34 +1,71 @@
-var formulario = document.querySelector("#form")
+const formulario = document.querySelectorAll("#form")[0];
+//Se reformula por una constante porque es un valor que no va a cambiar
+//Se agregan las variables al principio de JS para una mejor lectura
+const nombreForm= document.getElementById("name");
+const edadForm= document.getElementById("age");
+const nacionalidadForm= document.getElementById("nationality");
 
-formulario.onsubmit = function(e) {
-
-  e.prevent();
-  
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
-
-  var nombre = n.value
-  var edad = e.value
-
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
-
-  if (nombre.length === 0) {
-    n.classList.add("error")
+//Se agregan funciones para cada parametro para mejorar la validación en cada input
+function validarNombre(){
+  if(nombreForm.length === 0){
+    console.log("nombre no valido")
   }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
-  }
+};
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
+function validarEdad(){
+  if(edad < 18 || edad > 120){
+    console.log("edad mayor a 18")
   }
 }
+
+function agregarInvitado(){
+  if(nombre.length > 0 && (edad > 18  && edad < 120)){
+    console.log("agregar invitaods")
+  }
+}
+
+function validarFormulario(){
+  const nombreValido=validarNombre();
+  const edadValida=validarEdad();
+  const invitadoAgregado=agregarInvitado();
+}
+//Se agrega una función de addEventListener en submit, para mayor precisión
+formulario.addEventListener("submit", e =>{
+  e.preventDefault;
+  const formularioValid=validarFormulario();
+  if(formularioValid){
+    console.log("es valido")
+  }else{
+    console.log("no es valido")
+  }
+});
+
+  
+  // var n = formulario.elements[0]
+  // var e = formulario.elements[1]
+  // var na = formulario.elements[2]
+
+//   var nombre = n.value
+//   var edad = e.value
+
+//   var i = na.selectedIndex
+//   var nacionalidad = na.options[i].value
+//   console.log(nombre, edad)
+//   console.log(nacionalidad)
+
+//   if (nombre.length === 0) {
+//     n.classList.add("error")
+//   }
+//   if (edad < 18 || edad > 120) {
+//     e.classList.add("error")
+//   }
+
+// if (nombre.length > 0 
+//   && (edad > 18 
+//     && edad < 120) ) {
+//   agregarInvitado(nombre, edad, nacionalidad)
+//   }
+
 
 var botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
